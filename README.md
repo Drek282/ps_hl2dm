d57d735f54d53db1da1088dd2098e2918e2df78e
Drek <doxology@rogers.com>
2021-06-08 12:29:48 -0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,13 @@
+This is a module for PsychoStats containing all the files necessary to add support for Half-Life 2 Death Match.  This module will not do anything on it's own, you must first install the PsychoStats base, then install this module, in that order, to have working support for Half-Life 2 Death Match.
+
+You can find the base PsychoStats at the following link:
+
+[https://github.com/Drek282/PsychoStats](https://github.com/Drek282/PsychoStats "PsychoStats Base")
+
+
+## **What this Module Contains**
+
+* Code to add support for Half-Life 2 Death Match.
+* Images, such as maps, overlays, weapons and role images, specific to Half-Life 2 Death Match.
+* Any plugins specific to Half-Life 2 Death Match.
+* Any themes or child themes specific to Half-Life 2 Death Match.
---
+++ www/install/mysql/source/hl2dm.sql
@@ -0,0 +1,47 @@
+-- Moved from defaults.sql.
+INSERT INTO `ps_config_events` (`id`, `gametype`, `modtype`, `eventname`, `alias`, `regex`, `idx`, `ignore`, `codefile`) 
+    VALUES 
+        (1,'source','','kill','','/^\"(.+(?:<[^>]*>))\" killed \"(.+(?:<[^>]*>))\" with \"([^\"]*)\"(.*)/',10,0,''),
+        (2,'source','','attacked','','/^\"(.+(?:<[^>]*>))\" attacked \"(.+(?:<[^>]*>))\" with \"([^\"]+)\"(.*)/',20,1,''),
+        (3,'source','','plrtrigger','','/^\"(.+(?:<[^>]*>))\" triggered \"([^\"]+)\"(.*)/',30,0,''),
+        (4,'source','','round','','/^World triggered \"([^\"]+)\"(.*)/',40,0,''),
+        (5,'source','','entered_game','','/^\"(.+(?:<[^>]*>))\" entered the game(.*)/',50,0,''),
+        (6,'source','','joined_team','','/^\"(.+(?:<[^>]*>))\" joined team \"([^\"]+)\"/',60,0,''),
+        (7,'source','','suicide','','/^\"(.+(?:<[^>]*>))\" committed suicide with \"([^\"]+)\"(.*)/',70,0,''),
+        (8,'source','','changed_name','','/^\"(.+(?:<[^>]*>))\" changed name to \"([^\"]+)\"/',80,0,''),
+        (9,'source','','connected','','/^\"(.+(?:<[^>]*>))\" connected, address \"([^\"]+)\"/',90,0,''),
+        (10,'source','','connected_steamid','','/^\"(.+(?:<[^>]*>))\" (?:STEAM|VALVE) USERID (.+)/',100,0,''),
+        (11,'source','','disconnected','','/^\"(.+(?:<[^>]*>))\" disconnected/',110,0,''),
+        (12,'source','','chat','','/^\"(.+(?:<[^>]*>))\" say(_team)* \"(.*)\"?(.*)/',120,0,''),
+        (13,'source','','rcon','','/^(Bad )?Rcon: \"rcon (-*\\d+) \"?(.*?)\"? (.+?)(?:\" from \"([^\"]+)\")?/',130,1,''),
+        (14,'source','','cheated','','/^Secure: \"(.+(?:<[^>]*>))\" was detected cheating/',140,0,''),
+        (15,'source','','ban','','/^(Addip|Ban(?:id)?): \"([^\"]+)\" was (?:kicked and )?(?:banned)(?: by IP)? \"([^\"]+)\" by \"([^\"]+)\"(.*)/',150,1,''),
+        (16,'source','','unban','','/^Remove(id|ip): \"([^\"]+)\" was unbanned by \"(.+(?:<[^>]*>))\"(.*)/',160,1,''),
+        (17,'source','','kick','','/^Kick: \"(.+(?:<[^>]*>))\" was kicked by \"(.+(?:<[^>]*>)?)\"(.*)/',170,0,''),
+        (18,'source','','pingkick','','/^\"(.+(?:<[^>]*>))\" kicked due to high ping(.*)/',180,0,''),
+        (19,'source','','ffkick','','/^\"(.+(?:<[^>]*>))\" has been auto kicked from the game for TKing/',190,0,''),
+        (20,'source','','startedmap','','/^(Started|Loading) map \"([^\"]+)\"(.*)/',200,0,''),
+        (21,'source','','logstartend','','/^Log file (started|closed)(.*)/',210,0,''),
+        (22,'source','','ignored1','','/^[Ss]erver (?:cvars?|say|shutdown)/',220,1,''),
+        (23,'source','','ignored2','','/^(?:\\]TSC\\[|Succeeded|FATAL|-|Loaded)/',230,1,''),
+        (24,'source','','ignored3','','/^(?:Config|Swear|server_)/',240,1,''),
+        (25,'source','','ignored4','','/^\"[^\"]+\" = \"/',250,1,''),
+        (33,'source','','changed_role','','/^\"(.+(?:<[^>]*>))\" changed role to \"([^\"]+)\"/',330,0,''),
+        (45,'source','','rcon2','','/^rcon from/',130,1,''),
+        (48,'source','','spatial','','/^\\[KTRAJ\\] \"(.+(?:<[^>]*>))\" killed \"(.+(?:<[^>]*>))\" with \"([^\"]*)\"(.*)/',480,0,''),
+        (49,'source','','plugin','','/^\\[[^\\]]+\\]/',490,1,''),
+        (50,'source','','console','','/^(CONSOLE|PLAYER FILTER) :/',500,1,''),
+        (100,'source','','ignored5','','/^Server name is \"([^"]+)\"/',1000,1,''),
+        (101,'source','','ignored6','','/^Connection to Steam servers successful./',1010,1,''),
+        (102,'source','','ignored7','','/^VAC secure mode is activated./',1020,1,''),
+        (103,'source','','ignored8','','/^Assigned anonymous gameserver Steam ID \[(.:\\d:\\d+:\\d+)\]./',1030,1,''),
+        (104,'source','','ignored9','','/^Public IP is ([0-9A-Fa-f]{1,4}:){7}[0-9A-Fa-f]{1,4}|(\\d{1,3}\.){3}\\d{1,3}./',1040,1,'');
+
+INSERT INTO `ps_config_awards` (`id`, `enabled`, `idx`, `type`, `negative`, `class`, `name`, `groupname`, `phrase`, `expr`, `order`, `where`, `limit`, `format`, `gametype`, `modtype`, `rankedonly`, `description`) 
+    VALUES 
+        (4,1,10,'player',0,'','Highest Skill','','{$player.link} has the highest skill with {$award.value}','{$skill}','desc','',0,'%.02f',NULL,NULL,1,'Player with the highest skill'),
+        (6,1,20,'player',0,'','Most Kills','','{$player.link} has killed the most players ({$award.value})','{$kills}','desc','',0,'commify',NULL,NULL,1,'Player with the most kills (any team)'),
+        (8,1,70,'player',0,'','Most Time Online','','{$player.link} has been online the longest ({$award.value})','{$onlinetime}','desc','',25,'compacttime',NULL,NULL,1,'Player with the most online time'),
+        (9,1,190,'weaponclass',0,'','Most Kills with {$weapon.class} weapons','','{$player.link} has the most {$weapon.class} kills ({$award.value})','{$kills}','desc','',0,'commify',NULL,NULL,1,'Most kills with {$weapon.class} type weapons (including: {$weapon.list}).'),
+        (10,1,180,'weapon',0,'','Most Kills with {$weapon.name}','','{$player.link} has the most {$weapon.link} kills ({$award.value})','{$kills}','desc','',0,'commify',NULL,NULL,1,'Most kills with a {$weapon.name}'),
+        (13,1,30,'player',0,'','Most Bonus Points','','{$player.link} achieved the most bonus points ({$award.value})','{$totalbonus}','desc','',25,'commify',NULL,NULL,1,'Player that has achieved the most bonus points.');
